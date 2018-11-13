34787e3d0b37c50cb4100436b911374f4f22d29d
PatoKross <pato.jrs@gmail.com>
2018-11-12 22:22:56 -0300

    primer commit

---
+++ config/global.php
@@ -0,0 +1,9 @@
+<?php
+    define("DB_HOST","localhost");
+    define("DB_NAME","segemed");
+    define("DB_USERNAME","segemed_admin");
+    define("DB_PASSWORD","Passw0rd");
+    define("DB_ENCONDE","utf8");
+    define("PRO_NOMBRE","SEGEMED GASTOS");
+
+?>
